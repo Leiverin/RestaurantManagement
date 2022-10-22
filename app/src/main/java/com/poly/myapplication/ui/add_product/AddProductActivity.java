78a38feb087ccbbcc8e0bd8c53a46a5d86709c7d
tuananh13012002 <tuananh13160108@gamil.com>
2022-10-22 23:23:53 +0700

    tuananh

--- app/src/main/java/com/poly/myapplication/ui/add_product/AddProductActivity.java
+++ app/src/main/java/com/poly/myapplication/ui/add_product/AddProductActivity.java
@@ -8,25 +8,25 @@ import android.os.Bundle;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 import com.poly.myapplication.R;
-import com.poly.myapplication.databinding.ActivityAddProductBinding;
+import com.poly.myapplication.databinding.ActivityFoodBinding;
 import com.poly.myapplication.ui.add_product.adapter.ViewPager2Adapter;
 
 public class AddProductActivity extends AppCompatActivity {
-    private ActivityAddProductBinding binding;
+    private ActivityFoodBinding binding;
     public static String[] categories = new String[]{"Appetizer","Main dishes","Desserts","Drinks"};
     private ViewPager2Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityAddProductBinding.inflate(getLayoutInflater());
+        binding = ActivityFoodBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         adapter = new ViewPager2Adapter(this);
-        binding.viewpager2.setAdapter(adapter);
-        new TabLayoutMediator(binding.tabLayout,binding.viewpager2,((tab, position) -> {
+        binding.viewPager2Main.setAdapter(adapter);
+        new TabLayoutMediator(binding.tabLayout,binding.viewPager2Main,((tab, position) -> {
             tab.setText(categories[position]);
         })).attach();
-        binding.viewpager2.setCurrentItem(1,false);
+        binding.viewPager2Main.setCurrentItem(1,false);
 
 
 
