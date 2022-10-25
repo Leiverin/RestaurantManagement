b57620d6f949d1c03dd771682f38d11282148b7b
Leiverin <vanhungmob@gmail.com>
2022-10-25 22:29:46 +0700

    more

---
+++ app/src/main/java/com/poly/myapplication/data/models/TableParent.java
@@ -0,0 +1,32 @@
+package com.poly.myapplication.data.models;
+
+import java.util.List;
+
+public class TableParent {
+    private String title;
+    private List<Table> tables;
+
+    public TableParent() {
+    }
+
+    public TableParent(String title, List<Table> tables) {
+        this.title = title;
+        this.tables = tables;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Table> getTables() {
+        return tables;
+    }
+
+    public void setTables(List<Table> tables) {
+        this.tables = tables;
+    }
+}
--- app/src/main/java/com/poly/myapplication/ui/activities/manage/adapters/TableManageAdapter.java
+++ app/src/main/java/com/poly/myapplication/ui/activities/manage/adapters/TableManageAdapter.java
@@ -1,15 +1,51 @@
 package com.poly.myapplication.ui.activities.manage.adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class TableManageAdapter{
-    public class TableManageViewHolder extends RecyclerView.ViewHolder{
+import com.poly.myapplication.data.models.Table;
+import com.poly.myapplication.data.models.TableParent;
+import com.poly.myapplication.databinding.ItemTableParentBinding;
 
-        public TableManageViewHolder(@NonNull View itemView) {
-            super(itemView);
+import java.util.List;
+
+public class TableManageAdapter extends RecyclerView.Adapter<TableManageAdapter.TableManageViewHolder> {
+    private List<TableParent> mListTable;
+    private Context context;
+
+    public TableManageAdapter(List<TableParent> mListTable, Context context) {
+        this.mListTable = mListTable;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public TableManageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new TableManageViewHolder(ItemTableParentBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TableManageViewHolder holder, int position) {
+        TableParent table = mListTable.get(position);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mListTable.size();
+    }
+
+    public class TableManageViewHolder extends RecyclerView.ViewHolder{
+        private ItemTableParentBinding binding;
+        public TableManageViewHolder(ItemTableParentBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
         }
     }
+
 }
