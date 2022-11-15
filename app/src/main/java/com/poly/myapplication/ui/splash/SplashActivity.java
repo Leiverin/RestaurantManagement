b5f7af03d366914ff6521b7e12ea8c364357697c
nghiapt98 <102583729+nghiapt98@users.noreply.github.com>
2022-11-15 21:34:05 +0700

    splashscreen

--- app/src/main/java/com/poly/myapplication/ui/splash/SplashActivity.java
+++ app/src/main/java/com/poly/myapplication/ui/splash/SplashActivity.java
@@ -2,9 +2,12 @@ package com.poly.myapplication.ui.splash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.poly.myapplication.R;
+import com.poly.myapplication.ui.login.LoginActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -12,6 +15,14 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
     }
 
 }
